8ec0f57e29f3a230325ce14d39ad9823b4067987
MaryWenzel <wenzel.mary1993@gmail.com>
2021-03-20 20:37:06 -0500

    test 2 push

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use("/recipes", recipesRouter);
 
 app.get("/", (req, res) => {
-  res.send("backend says hello!");
+  res.send("Welcome to my server for fooooooooooood!");
 });
 
 app.listen(port, () => console.log(`Example app running on port ${port}`));
